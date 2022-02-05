52280cbd37e31f038f59782d672a0d7cb6e9e157
Juangrcode <j3045771061@gmail.com>
2022-02-05 18:45:25 -0500

    [Update] file requirements

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edysan.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edysan.settings")
 
 application = get_wsgi_application()
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
